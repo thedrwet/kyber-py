f29d343a924c93ef1c4f0590568f0fdb5d95e87f
Abhisek Mohanty <thedrwet@gmail.com>
2025-02-05 00:51:42 +0530

    opyional

--- kyber-py/test.py
+++ kyber-py/test.py
@@ -5,11 +5,11 @@ def test_kyber512(name):
     c, key = Kyber512.enc(pk)
     _key = Kyber512.dec(c, sk)
     assert key == _key
-    print(f"{name} test passed!")
-    print(f"Public Key: {pk}")
-    print(f"Secret Key: {sk}")
-    print(f"Ciphertext: {c}")
-    print(f"Shared Key: {key}")
+    print(f"{name} test passed!\n \n \n")
+    print(f"Public Key: {pk}\n \n")
+    print(f"Secret Key: {sk}\n \n")
+    print(f"Ciphertext: {c} \n \n")
+    print(f"Shared Key: {key}\n \n")
     print("-" * 80)
 
 if __name__ == "__main__":
