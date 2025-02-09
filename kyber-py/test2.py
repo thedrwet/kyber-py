85b736852acf93eb31fca6515f1934008c9024c5
Abhisek Mohanty <thedrwet@gmail.com>
2025-02-09 21:09:06 +0530

    Update test2.py

--- kyber-py/test2.py
+++ kyber-py/test2.py
@@ -1,4 +1,5 @@
 from kyber_py.ml_kem import ML_KEM_512
+from kyber_py.modules.modules import ModuleKyber
 ek, dk = ML_KEM_512.keygen()
 key, ct = ML_KEM_512.encaps(ek)
 _key = ML_KEM_512.decaps(dk, ct)
