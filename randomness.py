5916752ad355f38a63ea37ecfff3267ca46a968f
Abhisek Mohanty <thedrwet@gmail.com>
2025-02-11 16:38:51 +0530

    normal

--- randomness.py
+++ randomness.py
@@ -1,9 +1,19 @@
 import random
 import matplotlib.pyplot as plt
-import kyber_py.polynomials.polynomials as poly #importing the polynomials module from the kyber_py package
+import kyber_py.polynomials.polynomials as poly  # importing the polynomials module from the kyber_py package
+import os
 
 # Generate a list of random floats
-polynomial = poly.Polynomial(3, 5) #creating a polynomial object with degree 3 and modulus 5
-print(polynomial) #printing the polynomial object
+# creating a polynomial object with degree 3 and modulus 5
+# printing the polynomial object
+i = os.urandom(3)
+a=[]
+a.append(i[:16])
+a.append(i[16:32])
+print(a)
+# Create an instance of the PolynomialRingKyber class
+poly_instance = poly.PolynomialRingKyber()
 
+# Call the ntt_sample method with the required input_bytes argument
+a = poly_instance.ntt_sample(input_bytes=i)
 
