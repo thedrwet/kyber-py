88f00d1a3ee853a39416ed30a0f27c10d95e11a5
Abhisek Mohanty <thedrwet@gmail.com>
2025-02-11 20:20:27 +0530

    change is required

--- kyber-py/test.py
+++ kyber-py/test.py
@@ -1,9 +1,8 @@
 
-from kyber import Kyber512, Kyber768, Kyber1024
+from kyber_py.kyber import  Kyber512
 pk, sk = Kyber512.keygen()
 key, c = Kyber512.encaps(pk)
 _key = Kyber512.decaps(sk, c)
 assert key == _key
 print("Kyber512 test passed")
-print(pk)
-print(sk)
+
